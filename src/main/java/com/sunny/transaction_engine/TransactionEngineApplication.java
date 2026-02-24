285653bb59e3e33a9ad6414951880211936d1fb4
unyksunny <unyksunny@gmail.com>
2026-02-24 06:59:27 +0530

    feat(bootstrap): initialized sb foundation with profiles

---
+++ src/main/java/com/sunny/transaction_engine/TransactionEngineApplication.java
@@ -0,0 +1,13 @@
+package com.sunny.transaction_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunny/transaction_engine/TransactionEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunny.transaction_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
